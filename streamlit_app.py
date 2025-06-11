58e4c3ce9ab6e9eaf78bbd0ce248a4999b03dbf8
lauricewardini <lauricewardini@gmail.com>
2025-06-11 13:08:16 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -80,7 +80,7 @@ def create_summary_page(date, item_summary):
 
 if uploaded_file is not None:
     try:
-        vendor_df = pd.read_csv(GOOGLE_SHEET_CSV_URL, skiprows=1))
+        vendor_df = pd.read_csv(GOOGLE_SHEET_CSV_URL, skiprows=1)
         vendor_df.columns = [c.strip().lower() for c in vendor_df.columns if c.strip()]
         vendor_df = vendor_df.dropna(subset=['vendor name'])
 
